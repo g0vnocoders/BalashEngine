8f75f5af23d147ce9fc9b17650d146f8f5f7402f
Denis <denistesla2005@gmail.com>
2021-04-26 18:22:22 +0300

    added png loader

--- include/linux.hpp
+++ include/linux.hpp
@@ -3,3 +3,5 @@ void platspec_initkeyboard();
 void platspec_sync();
 void platspec_creategamethread(void*(*func)(void*));
 void platspec_sleep(double);
+unsigned int * platspec_loadTexture(const char * filename,unsigned int width,unsigned int height);
+
